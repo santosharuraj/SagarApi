16fadb99e02467328d9cc3de07a19095209c1809
santosh.kumar14 <santosh.kumar@chat360.io>
2023-02-24 21:44:56 +0530

    changes

--- index.js
+++ index.js
@@ -2,17 +2,18 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userModal from "./model/userModal.js";
 import userRoute from "./routes/userRoute.js";
+
+dotenv.config();
 const app = express();
 app.use(express.json({ extented: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 const PORT = process.env.PORT || 4000;
-const MongoDB_URL = "mongodb://localhost:27017/DBName";
+
 mongoose.set("strictQuery", false);
 mongoose
-    .connect(MongoDB_URL, { useNewUrlParser: true })
+    .connect(process.env.MongoDB_URL, { useNewUrlParser: true })
     .then((result) => {
         app.listen(PORT, () => {
             console.log(`Port is running at ${PORT}`);
